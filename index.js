8eac36790bb07fd14021eb1d280ffd73082a4bf8
Andrew S <andrews54757@gmail.com>
2016-11-27 21:14:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 "use strict"
 module.exports = class AsyncConsole {
+constructor() {
+this.stdin = process.stdin;
+this.stdin.setRawMode(true);
+this.stdin.resume();
+this.stdin.setEncoding('utf8');
+  this.stdin.on('data',function(key) {
+this.onKey(key)
+    
+  }.bind(this)) 
+}
 
-
-
+onKey(key) {
+  
+  
+}
 
 
 }
