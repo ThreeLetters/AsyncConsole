d00cd45ab49253e8943a9b321bd1406dcdb793b1
Andrew S <andrews54757@gmail.com>
2016-11-27 22:23:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 "use strict"
+/*
+    AsyncConsole - Simple async console command helper
+    Copyright (C) 2016 Andrew S
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 var EOL = require('os').EOL
 module.exports = class AsyncConsole {
 constructor(call) {
@@ -100,8 +114,10 @@ onKey(key) {
      back() {
     if (this.text.length == 0) return;
         
-        this.text.splice(this.cursor.x-1,1) 
+        if (this.cursor.x > 0) {
+          this.text.splice(this.cursor.x-1,1) 
       this.cursor.x --
+        }
         this.log(this.fill('\r>' + this.text.join("")))
            this.sendOrig() 
   }
